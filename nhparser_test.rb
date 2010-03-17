ff9d58f2c3e22e8bda2f09ff00c108785e11308d
Brian Jordan <bjordan@OLPCXO.local>
2010-03-17 17:10:20 -0400

    More tests for nhparser

--- nhparser_test.rb
+++ nhparser_test.rb
@@ -7,25 +7,45 @@ require 'nhparser'
 require 'data_layer'
 
 class ParserTest < Test::Unit::TestCase
-  context "A single parser instance" do
-    should "open a ballot info text file" do
-      gen = DataLayer.new
-      par = NHParser.new("inputs/tinyballot.txt", gen)      
+  context "A parser instance on a single ballot" do
+    setup do
+      @gen = DataLayer.new
+      @par = NHParser.new("inputs/tinyballot.txt", @gen)
+      @par.parse_file
+      @single_ballot = @gen.h_file[0]
     end
     
-    should "parse a ballot info text file" do
-      gen = DataLayer.new
-      par = NHParser.new("inputs/tinyballot.txt", gen)
-      par.parse_file
-      assert true unless gen.h_file.nil? # generated file
-      assert_equal 1, gen.h_file.length # 1 ballot
-      
-      single_ballot = gen.h_file[0]
-
-      assert_equal "jurisdiction_slate", single_ballot["Audit-header"]["type"]
-      assert_equal 2, single_ballot["contest_list"].length
-      assert_equal  "President and Vice-President of the United States",
-                    single_ballot["contest_list"][0]["display_name"]
+    should "have one ballot" do
+      assert true unless @gen.h_file.nil? # generated file
+      assert_equal 1, @gen.h_file.length # 1 ballot
     end
+    
+    should "set ballot type" do
+      assert_equal "jurisdiction_slate", @single_ballot["Audit-header"]["type"]
+    end
+    
+    should "contain two contests, have display names" do
+      assert_equal 2, @single_ballot["contest_list"].length
+      assert_equal  "President and Vice-President of the United States",
+                    @single_ballot["contest_list"][0]["display_name"] 
+    end
+  end
+  
+  context "A parser instance on two ballots" do
+    setup do
+      @gen = DataLayer.new
+      @par = NHParser.new("inputs/mediumballot.txt", @gen)
+      @par.parse_file
+      @ballot1 = @gen.h_file[0]
+      @ballot2 = @gen.h_file[1]
+    end
+    
+    should "contain two ballots with contests" do
+      assert_equal  "President and Vice-President of the United States",
+                    @ballot1["contest_list"][0]["display_name"]
+      assert_equal  "Governor",
+                    @ballot2["contest_list"][1]["display_name"]                    
+    end
+    
   end
 end
